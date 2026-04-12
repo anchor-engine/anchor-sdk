331cb1e36cbaea5d9a473e4c9c8f4f2670c3739a
HasRahm <rahmanhasin5@gmail.com>
2026-04-12 18:59:11 -0400

    v0.1.0: Initial release of @anchor-engine/sdk

--- dist/errors.d.ts
+++
@@ -1,8 +0,0 @@
-import { ScoreResult } from './types';
-export declare class AnchorError extends Error {
-    constructor(message: string);
-}
-export declare class AnchorDriftError extends AnchorError {
-    result: ScoreResult;
-    constructor(message: string, result: ScoreResult);
-}
